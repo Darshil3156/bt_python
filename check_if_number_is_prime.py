3b434204fb6e926b952a3533f76f663fd32354f5
Darshil3156 <87858458+Darshil3156@users.noreply.github.com>
2022-10-09 13:21:06 +0530

    Update check_if_number_is_prime.py

--- check_if_number_is_prime.py
+++ check_if_number_is_prime.py
@@ -1,8 +1,8 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2639
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
-
-\f0\fs24 \cf0 hi}
+x = input("Enter a integer number: ")
+dividend = []
+myList = range(1, int(x) + 1)
+print ("my list is => ", myList)
+for y in myList:
+    if (int(x) % y) == 0:
+        dividend.append(y)
+print("List of divisors of provided nubmer", x, " is =>  ", dividend)
